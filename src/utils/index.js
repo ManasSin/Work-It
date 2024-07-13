66f6aad5e6dc580209de0125a19292f5565f9a46
ManasSin <maltisadan12@gmail.com>
2024-07-13 17:50:11 +0530

    :sparkles: chore: Upgraded the project to next js latest version and Starts using typescript instead of Javascript.
    This commit changes/resets the project to ground zero. As mentioned, i've started using next js instead of plain React and TS on the place of JS.

--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-// import { FiChevronDown, FiSettings } from "react-icons/fi";
-
-// const arrowDownSvg = <FiChevronDown width="15px" height="15px" />;
-
-// const settingSvg = <FiSettings width="15px" height="15px" />;
-
-// export default { settingSvg, arrowDownSvg };
