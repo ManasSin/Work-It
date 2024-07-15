f38036a092d888f6a4e8233e2309dcc78ab9777d
ManasSin <maltisadan12@gmail.com>
2024-07-15 18:31:45 +0530

    :sparkles: CHORE: Update UserProfile component to include dropdown menu for user actions
    The UserProfile component has been updated to include a dropdown menu for user actions. This allows users to sign out of the application by clicking the "Sign Out" button in the dropdown menu

--- components/nav/UserProfile.tsx
+++ components/nav/UserProfile.tsx
@@ -1,29 +1,58 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { SignOutAction } from "@/actions/auth-action";
 
 type Props = {};
 
 const UserProfile = (props: Props) => {
   const session = useSession();
-  console.log(session);
   if (session?.status === "loading") return <p>Loading...</p>;
-  if (session?.status === "unauthenticated") return <p>Unauthenticated</p>;
+  if (session?.status === "unauthenticated")
+    return <p className="truncate">Unauthenticated</p>;
   const imageUrl = session?.data?.user?.image;
-  const name = session?.data?.user?.name || "User";
+  const name = session?.data?.user?.name;
+  const email = session?.data?.user?.email || `${name}'s email`;
   return (
-    <div className="flex items-center justify-start gap-1 lg:gap-2 m-0 p-0 lg:px-3 lg:w-full bg-white">
-      {imageUrl && (
-        <Image
-          src={imageUrl}
-          alt="User Profile"
-          width={50}
-          height={50}
-          className="rounded-full flex-shrink justify-self-auto"
-        />
-      )}
-      <p className="truncate">{name}</p>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="hover:cursor-pointer">
+        <Button
+          variant={"secondary"}
+          className="flex items-center justify-start gap-1 lg:gap-2 m-0 p-0 lg:px-3 lg:w-full bg-white"
+        >
+          {imageUrl && (
+            <Image
+              src={imageUrl}
+              alt={`${email} profile picture`}
+              width={24}
+              height={24}
+              className="rounded-full flex-shrink justify-self-auto ring-4 "
+            />
+          )}
+          <p className="truncate">{email}</p>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuItem className="lg:w-full flex items-center justify-center">
+          <form action={SignOutAction}>
+            <Button
+              variant={"ghost"}
+              size={"lg"}
+              className="w-full h-full hover:text-primary p-2 text-center"
+            >
+              Sign Out
+            </Button>
+          </form>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
--- components/nav/sideBar.tsx
+++ components/nav/sideBar.tsx
@@ -17,10 +17,12 @@ export function Sidebar() {
     <div className="hidden border-r bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-          <Link href="/" className="flex items-center gap-2 font-semibold">
-            <UserProfile />
-          </Link>
-          <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+          <UserProfile />
+          <Button
+            variant="link"
+            size="icon"
+            className="ml-auto h-8 w-8 basis-full "
+          >
             <Bell className="h-4 w-4" />
             <span className="sr-only">Toggle notifications</span>
           </Button>
