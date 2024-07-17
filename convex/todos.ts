a7c48f2f139924bc5bc8fb1f5d201b58a50444bf
ManasSin <maltisadan12@gmail.com>
2024-07-17 14:22:29 +0530

    feat: Add labels, todos and projects modules to fetch and insert data in/out of database.
    This commit adds the labels and projects modules to the Convex API. The labels module provides query functions for retrieving labels and the projects module provides query functions for retrieving projects.

--- convex/todos.ts
+++ convex/todos.ts
@@ -1,4 +1,6 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { Id } from "./_generated/dataModel";
 
 export const get = query({
   args: {},
@@ -24,5 +26,63 @@ export const inCompleteTodos = query({
       .query("todos")
       .filter((q) => q.eq(q.field("isCompleted"), false))
       .collect();
+  },
+});
+
+export const totalTodos = query({
+  args: {},
+  handler: async (ctx) => {
+    const todos = await ctx.db.query("todos").collect();
+    return todos.length || 0;
+    return 0;
+  },
+});
+
+export const createATodo = mutation({
+  args: {
+    taskName: v.string(),
+    description: v.optional(v.string()),
+    priority: v.number(),
+    dueDate: v.number(),
+    projectId: v.id("projects"),
+    labelId: v.id("labels"),
+  },
+  handler: async (
+    ctx,
+    { taskName, description, priority, dueDate, projectId, labelId }
+  ) => {
+    try {
+      const newTaskId = await ctx.db.insert("todos", {
+        userId: "jn78c1x0enbmbbgm69d44z8trn6wyaeg" as Id<"users">,
+        taskName,
+        description,
+        priority,
+        dueDate,
+        projectId,
+        labelId,
+        isCompleted: false,
+      });
+      return newTaskId;
+    } catch (err) {
+      console.log("Error occurred during createATodo mutation", err);
+
+      return "";
+    }
+  },
+});
+
+export const checkATodo = mutation({
+  args: { taskId: v.id("todos") },
+  handler: async (ctx, { taskId }) => {
+    const newTaskId = await ctx.db.patch(taskId, { isCompleted: true });
+    return newTaskId;
+  },
+});
+
+export const unCheckATodo = mutation({
+  args: { taskId: v.id("todos") },
+  handler: async (ctx, { taskId }) => {
+    const newTaskId = await ctx.db.patch(taskId, { isCompleted: false });
+    return newTaskId;
   },
 });
