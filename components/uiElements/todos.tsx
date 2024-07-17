d031030cd5add202e12007ee5700e4afe1eec856
ManasSin <maltisadan12@gmail.com>
2024-07-17 14:37:44 +0530

    :boom: CHORE: improve functionality of todo list in Index page.
    This commit brings new enhanced feature to todo list, which lets user to mark the task complete or undo completed tasks. while also improve user experience.

---
+++ components/uiElements/todos.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Doc } from "@/convex/_generated/dataModel";
+import TaskPro from "./taskPro";
+// import { useToast } from "../ui/use-toast";
+
+export default function Todos({ items }: { items: Array<Doc<"todos">> }) {
+  const checkATodo = useMutation(api.todos.checkATodo);
+  const unCheckATodo = useMutation(api.todos.unCheckATodo);
+
+  const handleOnChangeTodo = (task: Doc<"todos">) => {
+    if (task.isCompleted) {
+      unCheckATodo({ taskId: task._id });
+    } else {
+      checkATodo({ taskId: task._id });
+    }
+  };
+  return items.map((task: Doc<"todos">, idx: number) => (
+    <TaskPro
+      key={task._id}
+      data={task}
+      isCompleted={task.isCompleted}
+      handleOnChange={() => handleOnChangeTodo(task)}
+    />
+  ));
+}
