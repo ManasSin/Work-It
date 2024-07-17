d64fb313f5b0ecd61ef2d68cd79346eb6a10d3b3
ManasSin <maltisadan12@gmail.com>
2024-07-17 14:39:11 +0530

    :lipstick: REFACTOR: displays new improved tasklist and add task modal on the main screen.

--- components/uiPages/todoList.tsx
+++ components/uiPages/todoList.tsx
@@ -1,42 +1,44 @@
 "use client";
+
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 import React from "react";
 import Task from "../uiElements/task";
+import Todos from "../uiElements/todos";
+import CompletedTodos from "../uiElements/completedTodos";
+import { AddTaskWrapper } from "../uiElements/AddTask/AddTaskButton";
 
 type Props = {};
 
 const TodoList = (props: Props) => {
   const todos = useQuery(api.todos.get) ?? [];
-  const completdTodos = useQuery(api.todos.CompletedTodos) ?? [];
+  const completedTodos = useQuery(api.todos.CompletedTodos) ?? [];
   const inCompleteTodos = useQuery(api.todos.inCompleteTodos) ?? [];
+  const totalTodos = useQuery(api.todos.totalTodos) ?? 0;
 
   if (
     todos === undefined ||
-    completdTodos === undefined ||
+    completedTodos === undefined ||
     inCompleteTodos === undefined
   ) {
     <p>Loading...</p>;
   }
-
   return (
-    <div className="xl:px-40">
+    // <div className="flex justify-center w-full">
+    <div className="w-[90%] max-w-[1480px] mx-auto">
       <div className="flex items-center justify-between">
         <h1 className="text-lg font-semibold md:text-2xl">Inbox</h1>
       </div>
-
-      <div className="flex flex-col gap-4 py-4">
-        <div className="flex flex-col items-center gap-2">
-          {inCompleteTodos?.map((task, idx) => (
-            <Task {...task} key={task._id} />
-          ))}
-        </div>
-
-        <div className="flex flex-col items-center gap-2">
-          {completdTodos?.map((task, idx) => <Task {...task} key={task._id} />)}
-        </div>
+      <div className="flex flex-col gap-1 py-4">
+        <Todos items={inCompleteTodos} />
       </div>
+      <AddTaskWrapper />
+      <div className="flex flex-col gap-1 py-4">
+        <Todos items={completedTodos} />
+      </div>
+      <CompletedTodos totalTodos={totalTodos} />
     </div>
+    // </div>
   );
 };
 
