c2880350cae0d1ae26ba6fec7cb74d60131663ff
ManasSin <maltisadan12@gmail.com>
2024-07-20 01:30:35 +0530

    :art: UI_FIX: text not displaying in dark mode fixed now.

--- components/uiElements/AddTask/AddTaskDialog.tsx
+++ components/uiElements/AddTask/AddTaskDialog.tsx
@@ -70,14 +70,16 @@ export default function AddTaskDialog({ data }: { data: Doc<"todos"> }) {
 
   return (
     <DialogContent className="max-w-4xl lg:h-4/6 flex flex-col md:flex-row lg:justify-between text-right h-fit">
-      <DialogHeader className="w-full">
+      <DialogHeader className="w-full grow">
         <DialogTitle>{taskName}</DialogTitle>
         <DialogDescription>
           <p className="my-2 capitalize">{description}</p>
-          <div className="flex items-center gap-1 mt-12 border-b-2 border-gray-100 pb-2 flex-wrap sm:justify-between lg:gap-0 ">
+          <div className="flex items-center gap-1 mt-12 border-b-2 border-gray-100 pb-2 flex-wrap justify-between lg:gap-0 ">
             <div className="flex gap-1">
               <ChevronDown className="w-5 h-5 text-primary" />
-              <p className="font-bold flex text-sm text-gray-900">Sub-tasks</p>
+              <p className="font-bold flex text-sm text-gray-900 dark:text-white/70">
+                Sub-tasks
+              </p>
             </div>
             <div>
               <Button variant={"outline"}>Suggest Missing Tasks (AI) 💖</Button>
@@ -114,14 +116,16 @@ export default function AddTaskDialog({ data }: { data: Doc<"todos"> }) {
           </div>
         </DialogDescription>
       </DialogHeader>
-      <div className="flex flex-col gap-2 bg-gray-100 lg:w-1/2">
+      <div className="flex flex-col gap-2 bg-gray-100 md:w-1/2">
         {todoDetails.map(({ labelName, value, icon }, idx) => (
           <div
             key={`${value}-${idx}`}
             className="grid gap-2 p-4 border-b-2 w-full"
           >
-            <Label className="flex items-start">{labelName}</Label>
-            <div className="flex text-left items-center justify-start gap-2 pb-2">
+            <Label className="dark:text-black/80 flex items-start">
+              {labelName}
+            </Label>
+            <div className="dark:text-black/80 flex text-left items-center justify-start gap-2 pb-2">
               {icon}
               <p className="text-sm">{value}</p>
             </div>
