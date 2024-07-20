bd1c4d51583b6475bce01de06c2538342f55cf2b
ManasSin <maltisadan12@gmail.com>
2024-07-20 13:32:40 +0530

    :sparkles: FEAT: Add ProjectIdPage component and related UI elements

---
+++ app/loggedin/projects/[projectId]/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import MobileNav from "@/components/nav/mobileNav";
+import { Sidebar } from "@/components/nav/sideBar";
+import { AddTaskWrapper } from "@/components/uiElements/AddTask/AddTaskButton";
+import CompletedTodos from "@/components/uiElements/completedTodos";
+import Todos from "@/components/uiElements/todos";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
+
+export default function ProjectIdPage() {
+  const { projectId } = useParams<{ projectId: Id<"projects"> }>();
+
+  const inCompletedTodosByProject =
+    useQuery(api.todos.getInCompleteTodosByProjectId, {
+      projectId,
+    }) ?? [];
+  const completedTodosByProject =
+    useQuery(api.todos.getCompletedTodosByProjectId, {
+      projectId,
+    }) ?? [];
+
+  const project = useQuery(api.projects.getProjectByProjectId, {
+    projectId,
+  });
+  const projectTodosTotal = useQuery(api.todos.getTodosTotalByProjectId, {
+    projectId,
+  });
+
+  // @ts-ignore
+  const projectName = project?.name || "";
+
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <Sidebar />
+      <div className="flex flex-col">
+        <MobileNav />
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
+          <div className="flex items-center justify-between">
+            <h1 className="text-lg font-semibold md:text-2xl">{projectName}</h1>
+          </div>
+          <Todos items={inCompletedTodosByProject} />
+
+          <div className="pb-6">
+            <AddTaskWrapper />
+          </div>
+
+          <Todos items={completedTodosByProject} />
+          <div className="flex items-center space-x-4 gap-2 border-b-2 p-2 border-gray-100 text-sm text-foreground/80">
+            <CompletedTodos totalTodos={projectTodosTotal as number} />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
