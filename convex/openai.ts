4b5ab444d2b16ad3826436c13872006d2380af31
ManasSin <maltisadan12@gmail.com>
2024-07-21 11:37:06 +0530

    :sparkles: FEAT: Integrate OpenAI for suggesting missing to-do items
    This commit integrates OpenAI into the project by adding the necessary code changes. It imports the required dependencies, sets up the OpenAI client with the API key, and introduces the new action function `suggestMissingItemsWithAi`. This function utilizes the OpenAI chat completions API to generate suggestions for missing to-do items in a given project. The suggested items are then created as new to-do items in the database.

--- convex/openai.ts
+++ convex/openai.ts
@@ -6,11 +6,11 @@ import OpenAI from "openai";
 import { Id } from "./_generated/dataModel";
 
 const apiKey = process.env.OPEN_AI_KEY;
-const openai = new OpenAI({ apiKey });
-// const openai = new OpenAI({
-//   apiKey: "anykey",
-//   baseURL: "http://localhost:3040/vi",
-// });
+// const openai = new OpenAI({ apiKey });
+const openai = new OpenAI({
+  apiKey: "anykey",
+  baseURL: "http://localhost:3040/vi",
+});
 
 export const suggestMissingItemsWithAi = action({
   args: {
@@ -53,6 +53,7 @@ export const suggestMissingItemsWithAi = action({
 
       for (let i = 0; i < items.length; i++) {
         const { taskName, description } = items[i];
+        const embedding = await getEmbeddingsWithAI(taskName);
         await ctx.runMutation(api.todos.createATodo, {
           taskName,
           description,
@@ -60,6 +61,7 @@ export const suggestMissingItemsWithAi = action({
           dueDate: new Date().getTime(),
           projectId,
           labelId: AI_LABEL_ID as Id<"labels">,
+          embedding,
         });
       }
     }
@@ -119,6 +121,7 @@ export const suggestMissingSubItemsWithAi = action({
 
       for (let i = 0; i < items.length; i++) {
         const { taskName, description } = items[i];
+        const embedding = await getEmbeddingsWithAI(taskName);
         await ctx.runMutation(api.todos.createATodo, {
           taskName,
           description,
@@ -126,8 +129,42 @@ export const suggestMissingSubItemsWithAi = action({
           dueDate: new Date().getTime(),
           projectId,
           labelId: AI_LABEL_ID as Id<"labels">,
+          embedding,
         });
       }
     }
   },
 });
+
+export const getEmbeddingsWithAI = async (searchText: string) => {
+  if (!apiKey) {
+    throw new Error("Open AI Key is not defined");
+  }
+
+  const req = {
+    input: searchText,
+    model: "text-embedding-ada-002",
+    encoding_format: "float",
+  };
+
+  const response = await fetch("https://api.openai.com/v1/embeddings", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify(req),
+  });
+
+  if (!response.ok) {
+    const msg = await response.text();
+    throw new Error(`OpenAI Error, ${msg}`);
+  }
+
+  const json = await response.json();
+  const vector = json["data"][0]["embedding"];
+
+  console.log(`Embedding of ${searchText}: , ${vector.length} dimensions`);
+
+  return vector;
+};
