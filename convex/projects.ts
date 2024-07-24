9ae5098db9f1a001d56c9da6abed741e99186b2e
ManasSin <maltisadan12@gmail.com>
2024-07-24 12:31:56 +0530

    :sparkles: REFACTOR: Update projects.ts to include createAProject, deleteProject, and deleteProjectAndItsTasks mutations

--- convex/projects.ts
+++ convex/projects.ts
@@ -1,6 +1,8 @@
-import { query, mutation } from "./_generated/server";
+import { query, mutation, action } from "./_generated/server";
 import { v } from "convex/values";
 import { handleUserID } from "./auth";
+import { api } from "./_generated/api";
+import { Doc } from "./_generated/dataModel";
 
 export const getProjects = query({
   args: {},
@@ -12,7 +14,10 @@ export const getProjects = query({
       .filter((q) => q.eq(q.field("userId"), userId))
       .collect();
 
-    const systemProjects = await ctx.db.query("projects").collect();
+    const systemProjects = await ctx.db
+      .query("projects")
+      .filter((q) => q.eq(q.field("type"), "system"))
+      .collect();
 
     return [...userProjects, ...systemProjects];
   },
@@ -35,5 +40,83 @@ export const getProjectByProjectId = query({
     }
 
     return null;
+  },
+});
+
+export const createAProject = mutation({
+  args: {
+    name: v.string(),
+    description: v.string(),
+  },
+  handler: async (ctx, { name, description }) => {
+    try {
+      const userId = await handleUserID(ctx);
+      if (userId) {
+        const newTaskId = await ctx.db.insert("projects", {
+          userId,
+          name,
+          description,
+          type: "user",
+        });
+        return newTaskId;
+      }
+
+      return null;
+    } catch (err) {
+      console.log("Error occurred during createAProject mutation", err);
+
+      return null;
+    }
+  },
+});
+
+export const deleteProject = mutation({
+  args: {
+    projectId: v.id("projects"),
+  },
+  handler: async (ctx, { projectId }) => {
+    try {
+      const userId = await handleUserID(ctx);
+      if (userId) {
+        const taskId = await ctx.db.delete(projectId);
+        //query todos and map through them and delete
+
+        return taskId;
+      }
+
+      return null;
+    } catch (err) {
+      console.log("Error occurred during deleteProject mutation", err);
+
+      return null;
+    }
+  },
+});
+
+export const deleteProjectAndItsTasks = action({
+  args: {
+    projectId: v.id("projects"),
+  },
+  handler: async (ctx, { projectId }) => {
+    try {
+      const allTasks = await ctx.runQuery(api.todos.getTodosByProjectId, {
+        projectId,
+      });
+
+      const promises = Promise.allSettled(
+        allTasks.map(async (task: Doc<"todos">) =>
+          ctx.runMutation(api.todos.deleteATodo, {
+            taskId: task._id,
+          })
+        )
+      );
+      const statuses = await promises;
+
+      await ctx.runMutation(api.projects.deleteProject, {
+        projectId,
+      });
+    } catch (err) {
+      console.error("Error deleting tasks and projects", err);
+    }
   },
 });
